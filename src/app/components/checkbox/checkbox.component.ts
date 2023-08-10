5365b64dc89da2793cba01c5b7572d6167c8d46f
EmelyTarazonaPerez <emelydayannatp@ufps.edu.co>
2023-08-09 21:55:37 -0500

    news changes

--- src/app/components/checkbox/checkbox.component.ts
+++ src/app/components/checkbox/checkbox.component.ts
@@ -11,9 +11,9 @@ export class CheckboxComponent {
   filtro: any[] = []
   categories: Category[] = [
     { id: 1, name: 'technology' },
-    { id: 2, name: 'clothes' },
-    { id: 3, name: 'furniture' },
-    { id: 4, name: 'accessories' }
+    { id: 1, name: 'clothes' },
+    { id: 1, name: 'furniture' },
+    { id: 1, name: 'accessories' }
   ]
 
   verificar(item: string): void {
